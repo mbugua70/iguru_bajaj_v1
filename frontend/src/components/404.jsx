90d6494225b37cfe1dbe4f4cfae13d64379d524a
john mbugua <johnymbugua1997@gmail.com>
2024-09-09 23:35:37 +0300

    stylying 404 page button

--- frontend/src/components/404.jsx
+++ frontend/src/components/404.jsx
@@ -2,18 +2,20 @@ import {Link} from "react-router-dom"
 import HomeNavBar from "./homenavbar";
 
 const PageNotFound = () =>{
-    return(
-        <>
-        <HomeNavBar/>
+    return (
+      <>
+        <HomeNavBar />
         <div className="pagenotfound">
-            <span><i className="material-icons">error</i></span>
-            <p>Sorry,the page you are looking for cannot be found!!</p>
-            <Link to="/" className="waves-effect waves-light btn">Back to Home</Link>
+          <span>
+            <i className="material-icons">error</i>
+          </span>
+          <p>Sorry,the page you are looking for cannot be found!!</p>
+          <Link to="/" className="error-btn_page">
+            Back to Home
+          </Link>
         </div>
-
-        </>
-
-    )
+      </>
+    );
 }
 
 export default PageNotFound;
